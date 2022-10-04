a0980f73880b350e72a27d46e1ba5ef2a9a6ab36
jasonmosley21 <jdm449@drexel.edu>
2022-10-03 22:49:50 -0400

    finished desktop

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,24 @@
 import { gsap } from "gsap";
+
+gsap.from("#firstline",{duration: 2, alpha:0, x:-100});
+gsap.from("#secondline",{duration: 1, alpha:0, x:100});
+gsap.set("#btn i",{transformOrigin:"center bottom"});
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("mouseover",function(){
+    gsap.to("#btn",{duration:1, scale:1.5});
+
+    gsap.to("#btn i",{duration: .25, rotateY: 360});
+
+    gsap.to("#firstline",{duration: 1, scale: .75});
+    gsap.to("#secondline",{duration: 1, scale: .75});
+})
+
+btn.addEventListener("mouseout",function(){
+    gsap.to("#btn",{duration:1, scale:1});
+
+    gsap.to("#btn i",{duration: 1, rotateY:0});
+
+    gsap.to("#firstline",{duration: 1, scale: 1});
+    gsap.to("#secondline",{duration: 1, scale: 1});
+})
