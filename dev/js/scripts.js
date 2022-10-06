73e58763d900206765be9e9f52b202de7fc17ff1
jasonmosley21 <jdm449@drexel.edu>
2022-10-06 14:05:38 -0400

    finished

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,27 +1,44 @@
 import { gsap } from "gsap";
 gsap.set("#trails-btn i",{transformOrigin:"center bottom"});
 
-gsap.from("#first-line",{duration: 1, alpha:0, y:-100});
-gsap.from("#second-line",{duration: 1, alpha:0, y:-70, delay:0.5});
-gsap.from("#trails-btn",{duration: 1, alpha:0, y:50, delay:0.75});
-gsap.from("#trails-btn i",{duration: 1, alpha:0, rotation:180, delay:0.75});
+// function heroANIME(){
+//     var TL = gsap.timeline();
+//     TL.from("#first-line",{duration: 1, alpha:0, y:-100})
+//       .from("#second-line",{duration: 1, alpha:0, y:-70})
+//       .from("#trails-btn",{duration: 1, alpha:0, y:50})
+//       .from("#trails-btn i",{duration: 1, alpha:0, rotation:180});
+//     return TL;
+// }
+
+// var mainTL = gsap.timeline();
+// mainTL.add(heroANIME());
+var herosizenumber = 1;
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 800px)", () => {
+  // desktop setup code here...
+  herosizenumber = 2
+});
+
+mm.add("(max-width: 799px)", () => {
+  // mobile setup code here...
+  herosizenumber = 1.25
+});
 
 let trailBtn = document.querySelector("#trails-btn");
 
-trailBtn.addEventListener("mouseover",function(){
-    gsap.to("#trails-btn",{duration:1, scale:2});
 
-    gsap.to("#trails-btn i",{duration: 1, rotateY:180});
+var buttonANIME = gsap.timeline({paused:true});
+buttonANIME.to("#trails-btn",{duration:.5, scale:herosizenumber})
+    .to("#trails-btn i",{duration: .5, rotateY:180})
+    .to("#first-line",{duration: .5, alpha:0, y:50},"goaway")
+    .to("#second-line",{duration: .5, alpha:0, y:20},"goaway")
 
-    gsap.to("#first-line",{duration: 1, alpha:0, y:50});
-    gsap.to("#second-line",{duration: 1, alpha:0, y:20});
+    trailBtn.addEventListener("mouseover",function(){
+        buttonANIME.play();
 })
 
 trailBtn.addEventListener("mouseout",function(){
-    gsap.to("#trails-btn",{duration:1, scale:1});
-
-    gsap.to("#trails-btn i",{duration: 1, rotateY:0});
-
-    gsap.to("#first-line",{duration: 1, alpha:1, y:0});
-    gsap.to("#second-line",{duration: 1, alpha:1, y:0});
+    buttonANIME.reverse();
 })
