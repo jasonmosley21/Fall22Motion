6a8f0e24f3e44d926a7e1291e3a9ce06e800aa72
jasonmosley21 <jdm449@drexel.edu>
2022-10-06 13:13:07 -0400

    replaced files

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,24 +1,27 @@
 import { gsap } from "gsap";
+gsap.set("#trails-btn i",{transformOrigin:"center bottom"});
 
-gsap.from("#firstline",{duration: 2, alpha:0, x:-100});
-gsap.from("#secondline",{duration: 1, alpha:0, x:100});
-gsap.set("#btn i",{transformOrigin:"center bottom"});
-let btn = document.querySelector("#btn");
+gsap.from("#first-line",{duration: 1, alpha:0, y:-100});
+gsap.from("#second-line",{duration: 1, alpha:0, y:-70, delay:0.5});
+gsap.from("#trails-btn",{duration: 1, alpha:0, y:50, delay:0.75});
+gsap.from("#trails-btn i",{duration: 1, alpha:0, rotation:180, delay:0.75});
 
-btn.addEventListener("mouseover",function(){
-    gsap.to("#btn",{duration:1, scale:1.5});
+let trailBtn = document.querySelector("#trails-btn");
 
-    gsap.to("#btn i",{duration: .25, rotateY: 360});
+trailBtn.addEventListener("mouseover",function(){
+    gsap.to("#trails-btn",{duration:1, scale:2});
 
-    gsap.to("#firstline",{duration: 1, scale: .75});
-    gsap.to("#secondline",{duration: 1, scale: .75});
+    gsap.to("#trails-btn i",{duration: 1, rotateY:180});
+
+    gsap.to("#first-line",{duration: 1, alpha:0, y:50});
+    gsap.to("#second-line",{duration: 1, alpha:0, y:20});
 })
 
-btn.addEventListener("mouseout",function(){
-    gsap.to("#btn",{duration:1, scale:1});
+trailBtn.addEventListener("mouseout",function(){
+    gsap.to("#trails-btn",{duration:1, scale:1});
 
-    gsap.to("#btn i",{duration: 1, rotateY:0});
+    gsap.to("#trails-btn i",{duration: 1, rotateY:0});
 
-    gsap.to("#firstline",{duration: 1, scale: 1});
-    gsap.to("#secondline",{duration: 1, scale: 1});
+    gsap.to("#first-line",{duration: 1, alpha:1, y:0});
+    gsap.to("#second-line",{duration: 1, alpha:1, y:0});
 })
