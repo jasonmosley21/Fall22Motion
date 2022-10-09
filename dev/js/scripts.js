c597cc662c22121994a26dceaf7735d292d56619
jasonmosley21 <jdm449@drexel.edu>
2022-10-09 11:44:56 -0400

    added timelines

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,24 +1,43 @@
 import { gsap } from "gsap";
 
-gsap.from("#firstline",{duration: 2, alpha:0, x:-100});
-gsap.from("#secondline",{duration: 1, alpha:0, x:100});
 gsap.set("#btn i",{transformOrigin:"center bottom"});
+
+function heroANIME(){
+  var TL = gsap.timeline();
+  TL.from("#firstline",{duration: 2, alpha:0, x:-100})
+    .from("#secondline",{duration: 1, alpha:0, x:100});
+  return TL;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(heroANIME());
+
+var herobtnsize = 1
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 845px)", () => {
+  // desktop setup code here...
+  herobtnsize = 1.5;
+});
+
+mm.add("(max-width: 844px)", () => {
+  // mobile setup code here...
+  herobtnsize = 1.75;
+});
+
 let btn = document.querySelector("#btn");
 
+var btnANIME = gsap.timeline({paused:true});
+btnANIME.to("#btn",{duration:1, scale: herobtnsize})
+        .to("#btn i",{duration: .25, rotateY: 360})
+        .to("#firstline",{duration: 1, scale: .75})
+        .to("#secondline",{duration: 1, scale: .75});
+
 btn.addEventListener("mouseover",function(){
-    gsap.to("#btn",{duration:1, scale:1.5});
-
-    gsap.to("#btn i",{duration: .25, rotateY: 360});
-
-    gsap.to("#firstline",{duration: 1, scale: .75});
-    gsap.to("#secondline",{duration: 1, scale: .75});
+  btnANIME.play();
 })
 
 btn.addEventListener("mouseout",function(){
-    gsap.to("#btn",{duration:1, scale:1});
-
-    gsap.to("#btn i",{duration: 1, rotateY:0});
-
-    gsap.to("#firstline",{duration: 1, scale: 1});
-    gsap.to("#secondline",{duration: 1, scale: 1});
+  btnANIME.reverse();
 })
