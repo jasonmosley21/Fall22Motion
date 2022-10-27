8b8ce8060729197b36683441430dd3d00e23d14d
jasonmosley21 <jdm449@drexel.edu>
2022-10-27 13:03:25 -0400

    tried the shockingly green stuff

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,6 +9,7 @@ function heroANIME(){
     .from("#latewinter h2", {scale:.5, duration:2, transformOrigin: "center bottom"},"size")
     .from("#latewinter p", {scale:.5, duration:2,transformOrigin: "center top"},"size")
     .from("#latewinter button", {scale:.5, duration:2,transformOrigin: "center top"},"size");
+    return tl;
 }
 
 function shopANIME(){
@@ -31,11 +32,13 @@ function shopANIME(){
     .from("#item16",{y:400},"fifthrow")
     .to("#viewall",{scale:2})
     .to("#viewall",{scale:1});
+    return tl;
 }
 
 function wonderANIME(){
     var tl = gsap.timeline({scrollTrigger:{trigger:"#wonder", scrub:true, markers: false, end:"10% 50%", start:"top 75%"}});
     tl.to("#wonder", {rotateY:360});
+    return tl;
 }   
 
 function sectionsANIME(){
@@ -54,11 +57,13 @@ function sectionsANIME(){
     tl.from("#section7", {scale:.5, duration:1.05},"row4")
     tl.to("#section6", {scale:.5},"rerow4")
     tl.to("#section7", {scale:.5},"rerow4");
+    return tl;
 }
 
 function dontseeANIME(){
     var tl = gsap.timeline({scrollTrigger:{trigger:"#dontsee", scrub:true, markers: false, end:"bottom 75%", start:"top 80%"}});
     tl.from("#dontsee article", {y:-400})
+    return tl;
 }
 
 var maintimeline = gsap.timeline();
