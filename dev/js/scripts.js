ab491006fb5b093343db25462432230e4029d95d
jasonmosley21 <jdm449@drexel.edu>
2022-11-16 20:55:17 -0500

    setup
    setting up functions and gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,42 @@
 import { gsap } from "gsap";
+
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
+
+function barsANIME(){
+    var tl = gsap.timeline()
+    
+    return tl;
+}
+
+function logoANIME(){
+    var tl = gsap.timeline()
+    
+    return tl;
+}
+
+function titleANIME(){
+    var tl = gsap.timeline()
+    
+    return tl;
+}
+
+var maintl = gsap.timeline();
+maintl.add(barsANIME())
+.add(logoANIME())
+.add(titleANIME());
+
+
+
+
+
+
+
+
+
+
+
+
+
+GSDevTools.create();
